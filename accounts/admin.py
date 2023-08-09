530723bb4db2d59d661dda673466603171fc5dcc
florafhc <fuhaichen0419@163.com>
2023-08-09 21:13:25 +0800

    account activation and password reset

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Account
 from django.contrib.auth.admin import UserAdmin
+from .models import Account
+from django.utils.html import format_html
 
 # Register your models here.
 
-# Make password in admin site readOnly
 class AccountAdmin(UserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
     list_display_links = ('email', 'first_name', 'last_name')
@@ -16,4 +16,3 @@ class AccountAdmin(UserAdmin):
     fieldsets = ()
 
 admin.site.register(Account, AccountAdmin)
-
